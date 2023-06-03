3c5b526dfe3bc8ae92ae9eccbbc568e19ffb07e3
muhnuryusri <muhnuryuusri@gmail.com>
2023-06-03 12:49:33 +0800

    add data source

--- lib/features/category_list/data/datasources/category_list_local_data_source.dart
+++ lib/features/category_list/data/datasources/category_list_local_data_source.dart
@@ -1,6 +1,35 @@
-import 'package:recepify/features/category_list/data/models/category_list_model.dart';
+import 'dart:convert';
+
+import 'package:recepify/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/category_list_model.dart';
+
+const CACHED_CATEGORY_LIST = 'CACHED_CATEGORY_LIST';
 
 abstract class CategoryListLocalDataSource {
   Future<CategoryListModel> getCachedCategoryList();
   Future<void> cacheCategoryList(CategoryListModel category);
+}
+
+class CategoryListLocalDataSourceImpl implements CategoryListLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  CategoryListLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<CategoryListModel> getCachedCategoryList() {
+    final jsonString = sharedPreferences.getString(CACHED_CATEGORY_LIST);
+    if (jsonString != null) {
+      return Future.value(CategoryListModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheExceptions();
+    }
+  }
+
+  @override
+  Future<void> cacheCategoryList(CategoryListModel category) {
+    final jsonString = json.encode(category.toJson());
+    return sharedPreferences.setString(CACHED_CATEGORY_LIST, jsonString);
+  }
 }
--- lib/features/category_list/data/datasources/category_list_remote_data_source.dart
+++ lib/features/category_list/data/datasources/category_list_remote_data_source.dart
@@ -1,5 +1,32 @@
-import 'package:recepify/features/category_list/data/models/category_list_model.dart';
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:recepify/core/error/exceptions.dart';
+
+import '../models/category_list_model.dart';
 
 abstract class CategoryListRemoteDataSource {
   Future<CategoryListModel> getCategoryList();
+}
+
+class CategoryListRemoteDataSourceImpl implements CategoryListRemoteDataSource {
+  final Dio client;
+
+  CategoryListRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<CategoryListModel> getCategoryList() async {
+    final response = await client.get(
+        'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
+        options: Options(headers: {'Content-Type': 'application/json'}));
+
+    if (response.statusCode == 200) {
+      final responseData = response.data;
+      final categoryListModel =
+          CategoryListModel.fromJson(json.decode(responseData));
+      return categoryListModel;
+    } else {
+      throw ServerExceptions();
+    }
+  }
 }
